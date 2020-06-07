df5ce22aa8ca9063c7cc74fa268a1237258cf985
Fran Jurmanović <fjurma12@outlook.com>
2020-06-07 15:33:17 +0200

    Boards layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'angular-boards';
+  lists = [
+    {
+      title: "To-Do",
+      items: [
+        {
+          content: "One",
+          labels: [
+            "Frontend"
+          ]
+        },
+        {
+          content: "Two",
+          labels: [
+            "Backend"
+          ]
+        }
+      ]
+    },
+    {
+      title: "In progress",
+      items: [
+        {
+          content: "Three",
+          labels: [
+            "Frontend",
+            "Javascript"
+          ]
+        }
+      ]
+    },
+    {
+      title: "Complete",
+      items: [
+        {
+          content: "Four",
+          labels: [
+            "Backend",
+            "ExpressJs"
+          ]
+        },
+        {
+          content: "Five",
+          labels: [
+            "Frontend",
+            "Javascript"
+          ]
+        }
+      ]
+    }
+  ];
+
+  
+
+  ngOnInit() {
+    
+  }
+
+  onDrop(event: CdkDragDrop<string[]>) {
+    console.log(event)
+  }
 }
